0d21f5a625fada33d25de3122057870971713e0c
Diana Vladotsenko <diana.vladotsenko@gmail.com>
2025-02-04 17:56:38 +0200

    added react to generate pages automatically

---
+++ public/src/app.js
@@ -0,0 +1,33 @@
+
+function staticContainer({}) {
+    return React.createElement("div", {className: "contact-basket-container", style: {marginLeft: "80px", marginRight: "80px"}},
+        React.createElement("p", {className: "pages-container"},
+        React.createElement("a", {className: "page-1", href: "/"}, "Home/"),
+        React.createElement("a", {className: "page-2", href: "/product"}, "Product")
+        ),
+        React.createElement("img",{src: "../icons/basket.svg", alt:"Basket Icon", className:"basket-icon"}) 
+    )
+}
+
+function pageProductSection({}) {
+    return React.createElement("div", {className: "page-product-container"},
+        React.createElement("div", {className: "page-product-images"},
+            React.createElement("img", {src: "/database/products/images/", alt: "Page Product Image", className: "product-image-small"}),
+            React.createElement("a", {className: "page-2", href: "/product"}, "Product")
+        ),
+        React.createElement("img",{src: "../icons/basket.svg", alt:"Basket Icon", className:"basket-icon"}) 
+    )
+}
+
+
+const App = ()=>{
+    return React.createElement(
+        "div", null, 
+        React.createElement(staticContainer),
+        React.createElement(pageProductSection),
+    )
+}
+
+const container= document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App)); 
