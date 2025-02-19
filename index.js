0a80cf80085a7f6d23fe49016c94d055aff2baad
Diana Vladotsenko <diana.vladotsenko@gmail.com>
2025-02-19 21:15:49 +0200

    .

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 import express from "express";
-const { static: serveStatic } = express;
 const app = express();
-
 app.set("view engine", "ejs");
-app.use(serveStatic("public"));
+app.use(express.static("public"));
+
 
 app.get("/", (req, res) => {
-    res.render("index");
+  res.render("index");
 });
 
 app.get("/product", (req, res) => {
-    res.render("product");
+  res.render("product");
 });
 
-console.log("Server is running on :http://localhost:5500")
+console.log("Server is running on :http://localhost:5500");
 app.listen(5500);
--- public/src/app.jsx
+++ public/src/app.jsx
@@ -1,19 +1,20 @@
 import { createRoot } from "react-dom/client";
-//import Pizza from "./Pizza";
+
 
 const App = () => {
     return (
-    <div className="a" style="display:none">
-        <div className = "contact-basket-container" style="margin-left:80px;margin-right:80px;">
-        <p className="pages-container">
-            <a className="page-1" href = "/">Home/</a>
-            <a className="page-2" href = "/product">Product</a>
-	    </p>
-	    <i className="bi-basket">
-		    <img src="/icons/basket.svg" alt="Basket Icon" className="basket-icon"/>
-	    </i>
-        </div>
-    </div>
+        <h1>Welcome to the Product Page</h1>
+    // <div className="a" style="display:none">
+    //     <div className = "contact-basket-container" style={"margin-left:80px;margin-right:80px;"}>
+    //     <p className="pages-container">
+    //         <a className="page-1" href = "/">Home/</a>
+    //         <a className="page-2" href = "/product">Product</a>
+	//     </p>
+	//     <i className="bi-basket">
+	// 	    <img src="/icons/basket.svg" alt="Basket Icon" className="basket-icon"/>
+	//     </i>
+    //     </div>
+    // </div>
     );
   };
   const container = document.getElementById("root");
